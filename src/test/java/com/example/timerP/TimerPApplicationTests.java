b89e23084518556a902d4ef38f99700cfbb0acee
SHIN SUJUNG <shinssjj79@gmail.com>
2024-10-26 20:44:49 +0900

    Initial commit

---
+++ src/test/java/com/example/timerP/TimerPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timerP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
